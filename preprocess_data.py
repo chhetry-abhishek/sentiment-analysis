827a98e205346c0d07e7472b3652f6250d3ad936
Abhishek Raut Chhetri <79572673+chhetry-abhishek@users.noreply.github.com>
2023-12-08 13:24:10 +1030

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,3 +1,4 @@
+
 import os
 import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -19,7 +20,7 @@ def determine_label(filename):
     elif 'negative' in filename:
         return 0
     else:
-        return -1  
+        return -1 
 
 for filename in os.listdir(cleaned_data_path):
     label = determine_label(filename)
@@ -31,32 +32,27 @@ for filename in os.listdir(cleaned_data_path):
                 reviews.append(review)
                 labels.append(label)
 
-
 combined = list(zip(reviews, labels))
 np.random.shuffle(combined)
 shuffled_reviews, shuffled_labels = zip(*combined)
-
 
 tokenizer = Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(shuffled_reviews)
 sequences = tokenizer.texts_to_sequences(shuffled_reviews)
 
 
-max_length = 100  
+max_length = 100 
 data = pad_sequences(sequences, maxlen=max_length, padding='post', truncating='post')
 
 
 X_train, X_test, y_train, y_test = train_test_split(data, shuffled_labels, test_size=0.2, random_state=42)
-
 
 np.save(os.path.join(output_path, 'X_train.npy'), X_train)
 np.save(os.path.join(output_path, 'X_test.npy'), X_test)
 np.save(os.path.join(output_path, 'y_train.npy'), y_train)
 np.save(os.path.join(output_path, 'y_test.npy'), y_test)
 
-
 import pickle
 with open(os.path.join(output_path, 'tokenizer.pickle'), 'wb') as handle:
     pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-print("Preprocessing complete. Files saved.")
