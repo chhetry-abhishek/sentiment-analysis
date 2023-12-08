cb55d55db34dc6ea48ded1504a820e146f51bea6
Abhishek Raut Chhetri <79572673+chhetry-abhishek@users.noreply.github.com>
2023-12-08 13:24:58 +1030

    Update process_text_data.py

--- process_text_data.py
+++ process_text_data.py
@@ -1,4 +1,5 @@
 
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -10,8 +11,7 @@ file_path = r'C:\Users\ab986\Desktop\sentiment_analysis_project\data\cleaned\cle
 with open(file_path, 'r', encoding='utf-8') as file:
     text_data = file.read()
 
-
-max_num_words = 10000 
+max_num_words = 10000  
 
 tokenizer = Tokenizer(num_words=max_num_words, oov_token='<OOV>')
 tokenizer.fit_on_texts([text_data])
@@ -20,11 +20,9 @@ word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences([text_data])
 
-max_sequence_length = 1000  
-
+max_sequence_length = 1000 
 
 padded_sequences = pad_sequences(sequences, maxlen=max_sequence_length, truncating='post', padding='post')
-
 
 print("Padded Sequence Example:")
 print(padded_sequences[0])
